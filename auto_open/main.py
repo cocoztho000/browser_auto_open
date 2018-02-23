de06042a57ed8ddf006bd1ef00bcaec3542a82bb
Tom Cocozzello <tjcocozz@us.ibm.com>
2018-02-23 16:51:34 -0600

    don't use selenium anymore

--- auto_open/main.py
+++ auto_open/main.py
@@ -1,4 +1,3 @@
-from selenium.webdriver import Firefox
 
 config = [
 	# Window 1
@@ -19,14 +18,33 @@ config = [
 	},
 ]
 
+import os
+import subprocess
+
+def get_display_res():
+	proc1 = subprocess.Popen(['xdpyinfo'], stdout=subprocess.PIPE)
+	proc2 = subprocess.Popen(['grep', 'dimensions'], stdin=proc1.stdout, stdout=subprocess.PIPE)
+	proc3 = subprocess.Popen(['awk', "{ print $2 }"], stdin=proc2.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	proc1.stdout.close() # Allow proc1 to receive a SIGPIPE if proc2 exits.
+	proc2.stdout.close() # Allow proc1 to receive a SIGPIPE if proc2 exits.
+	dim_x, dim_y = proc3.communicate()[0].split('x')
+
+	return int(dim_x.strip()), int(dim_y.strip())
+
+
+
+def run_cmd(url, browser_width, browser_height, window_position_x, window_position_y):
+	cmd = ('chromium --app="data:text/html,<html><body><script>window.moveTo(' + str(window_position_x) + ',' + str(window_position_y) +
+		');window.resizeTo('+ str(browser_width) +',' + str(browser_height) + ');window.location=\''+ url +'\';</script></body></html>" &')
+	print '... cmd: ' + cmd
+	os.system(cmd)
+
+width, height = get_display_res()
+
+
+
 for website in config:
 	print('Site --------------------------')
-	driver = Firefox()
-	driver.maximize_window()
-	window_size = driver.get_window_size()
-
-	width = window_size['width']
-	height = window_size['height']
 
 	print ('...Max Width:        ' + str(width))
 	print ('...Max Height:       ' + str(height))
@@ -43,6 +61,4 @@ for website in config:
 	print ('...Window Postion x: ' + str(window_position_x))
 	print ('...Window Postion y: ' + str(window_position_y))
 
-	driver.set_window_size(browser_width, browser_height)
-	driver.set_window_position(window_position_x, window_position_y)
-	driver.get(website['url'])
+	run_cmd(website['url'], browser_width, browser_height, window_position_x, window_position_y)
